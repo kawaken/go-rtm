be5497982eba1c3f71136feeb748aed006247e87
Kentaro Kawano <kentaro.kawano@gmail.com>
2016-12-22 12:43:08 +0900

    implement TestSign

--- request_test.go
+++ request_test.go
@@ -1,14 +1,23 @@
 package rtm
 
 import (
+	"net/url"
 	"testing"
 )
 
 func TestSign(t *testing.T) {
 
-	params := newParams()
+	// cf: https://www.rememberthemilk.com/services/api/authentication.rtm
+	params := url.Values{}
+	params.Add("yxz", "foo")
+	params.Add("feg", "bar")
+	params.Add("abc", "baz")
 
-	s := sign(params)
+	apiSecret = "BANANAS"
 
-	t.Log(s)
+	s := sign(params, apiSecret)
+
+	if s != "82044aae4dd676094f23f1ec152159ba" {
+		t.Fatalf("api sign is invalid result: %s", s)
+	}
 }
