d6bc4b30648a8b98c92258d18dceea7f815a04ea
Kentaro Kawano <kentaro.kawano@gmail.com>
2017-01-10 19:28:11 +0900

    add test

--- request_test.go
+++ request_test.go
@@ -1,8 +1,12 @@
 package rtm
 
 import (
+	"io/ioutil"
 	"net/url"
+	"os"
 	"testing"
+
+	"github.com/kawaken/go-rtm/methods/auth"
 )
 
 func TestSign(t *testing.T) {
@@ -18,4 +22,27 @@ func TestSign(t *testing.T) {
 	if s != "82044aae4dd676094f23f1ec152159ba" {
 		t.Fatalf("api sign is invalid result: %s", s)
 	}
+}
+
+func TestConnect(t *testing.T) {
+	client, err := NewClient(os.Getenv("RTM_API_KEY"), os.Getenv("RTM_API_SECRET"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := client.Do(auth.GetFrob())
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(string(b))
+
 }
